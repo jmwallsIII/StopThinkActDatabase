258c90a824e44e88cf4279bcc43a9aa8e7b01b80
John Walls <johnmurrellwalls@yahoo.com>
2022-06-21 13:20:36 -0400

    new file:   Program.cs 	new file:   StopThinkAct.csproj 	new file:   StopThinkAct.sln 	new file:   bin/Debug/net6.0/StopThinkAct.deps.json 	new file:   bin/Debug/net6.0/StopThinkAct.dll 	new file:   bin/Debug/net6.0/StopThinkAct.exe 	new file:   bin/Debug/net6.0/StopThinkAct.pdb 	new file:   bin/Debug/net6.0/StopThinkAct.runtimeconfig.json 	new file:   obj/Debug/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.cs 	new file:   obj/Debug/net6.0/StopThinkAct.AssemblyInfo.cs 	new file:   obj/Debug/net6.0/StopThinkAct.AssemblyInfoInputs.cache 	new file:   obj/Debug/net6.0/StopThinkAct.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net6.0/StopThinkAct.GlobalUsings.g.cs 	new file:   obj/Debug/net6.0/StopThinkAct.assets.cache 	new file:   obj/Debug/net6.0/StopThinkAct.csproj.AssemblyReference.cache 	new file:   obj/Debug/net6.0/StopThinkAct.csproj.BuildWithSkipAnalyzers 	new file:   obj/Debug/net6.0/StopThinkAct.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net6.0/StopThinkAct.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net6.0/StopThinkAct.dll 	new file:   obj/Debug/net6.0/StopThinkAct.genruntimeconfig.cache 	new file:   obj/Debug/net6.0/StopThinkAct.pdb 	new file:   obj/Debug/net6.0/apphost.exe 	new file:   obj/Debug/net6.0/ref/StopThinkAct.dll 	new file:   obj/Debug/net6.0/refint/StopThinkAct.dll 	new file:   obj/StopThinkAct.csproj.nuget.dgspec.json 	new file:   obj/StopThinkAct.csproj.nuget.g.props 	new file:   obj/StopThinkAct.csproj.nuget.g.targets 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StopThinkAct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this program is used to record \"Stop.Think.Act\" near miss observations.");
+            Console.WriteLine("Are you ready to continue?");
+            string Continue = Console.ReadLine();
+            if (Continue == "yes" || Continue == "Yes" || Continue == "yes " || Continue == "Yes ")
+            {
+                Console.WriteLine("What is your first and last name?");
+            }   
+            else
+            {
+                Console.WriteLine("You can terminate this program by exiting at the top right corner, or by pressing any key.");
+                Environment.Exit(0);
+            }
+            string Name = Console.ReadLine();
+            Console.WriteLine("Hello " + Name + ",");
+            Console.WriteLine("Please provide the date you witnessed the incident that you are reporting today; using mm/dd/yyyy format...");
+            string Dateobs = Console.ReadLine();
+            Console.WriteLine("is this the correct date? " + Dateobs + ". . . ");
+
+     
+
+            Console.ReadLine();
+        }
+    }
+}
