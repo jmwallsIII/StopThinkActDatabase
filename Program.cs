2aa263ededb022dafe2736de00d8fcd1c649f7d4
John Walls <johnmurrellwalls@yahoo.com>
2022-07-11 16:15:58 -0400

    modified:   .vs/ProjectEvaluation/stopthinkact.metadata.v2 	modified:   .vs/ProjectEvaluation/stopthinkact.projects.v2 	modified:   .vs/StopThinkAct/DesignTimeBuild/.dtbcache.v2 	new file:   .vs/StopThinkAct/FileContentIndex/17ac29cc-281d-4fea-99f4-380c71090879.vsidx 	deleted:    .vs/StopThinkAct/FileContentIndex/30e229aa-d4c7-450d-9929-0a24e88d3697.vsidx 	new file:   .vs/StopThinkAct/FileContentIndex/e18df111-d6b6-4f20-999b-b412d2891aae.vsidx 	modified:   .vs/StopThinkAct/v17/.suo 	modified:   Program.cs 	modified:   bin/Debug/net6.0/StopThinkAct.dll 	modified:   bin/Debug/net6.0/StopThinkAct.pdb 	new file:   bin/Debug/net6.0/example.txt 	modified:   obj/Debug/net6.0/StopThinkAct.dll 	modified:   obj/Debug/net6.0/StopThinkAct.pdb

--- Program.cs
+++ Program.cs
@@ -1,39 +1,90 @@
 ﻿    internal class Program
     {
+
+
         static void Main(string[] args)
         {
-        bool continuepar = false;
-            Console.WriteLine("Hello, this program is used to record \"Stop.Think.Act\" near miss observations.");
-            Console.WriteLine("Are you ready to continue?");
-        do
-        {
-            string Continue = Console.ReadLine();
-            if (Continue == "yes" || Continue == "Yes" || Continue == "yes " || Continue == "Yes ")
-            {
-                continuepar = true;
-                Console.WriteLine("What is your first and last name?");
-            }
+      
+            bool showMenu = true;
+                while (showMenu)
+                {
+                showMenu = MainMenu();
+                }
 
-            else if (Continue == "no" || Continue == "No" || Continue == "no " || Continue == "No ")
-            {
-                Console.WriteLine("You can terminate this program by exiting at the top right corner, or by pressing any key.");
-                Environment.Exit(0);
-            }
-            else
-            {
-                Console.WriteLine("Invalid response. Please use \"yes or no\" only");
-            }
-        } while (continuepar == false);
-
-
-        string Name = Console.ReadLine();
-            Console.WriteLine("Hello " + Name + ",");
-            Console.WriteLine("Please provide the date you witnessed the incident that you are reporting today; using mm/dd/yyyy format...");
-            string Dateobs = Console.ReadLine();
-            Console.WriteLine("is this the correct date? " + Dateobs + ". . . ");
-
-     
-
-            Console.ReadLine();
+            
         }
+        
+    
+        static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("MAIN MENU: STOP.THINK.ACT");
+            Console.WriteLine("1) ADD NEW EVENT");
+            Console.WriteLine("2) VIEW ALL EVENTS");
+            Console.WriteLine("3) EXIT APPLICATION");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())     //FIRST SWITCH STATEMENT
+            {
+                case "1":     
+                    Console.Clear ();
+                    Console.WriteLine("ADD NEW EVENT:");
+                    Console.WriteLine("1) Continue");
+                    Console.WriteLine("2) Return to MAIN MENU");
+                    Console.WriteLine("3) EXIT APPLICATION");
+                    Console.WriteLine("invalid option will revert to MAIN MENU");
+                    Console.Write("\r\nSelect an option: ");
+                        switch (Console.ReadLine())     //SECONDARY SWITCH STATEMENT
+                        {
+                        case "1":
+                            Console.Clear();
+                            Console.WriteLine("PLEASE INCLUDE THE FOLLOWING DETAILS:");
+                        File.AppendAllText(@"example.txt", Console.ReadLine() + Environment.NewLine);
+                        Console.ReadLine();
+                            return false;             
+                        case "2":
+                            return true;
+                        case "3":
+                            Console.Clear();
+                            Console.WriteLine("PRESS ANY KEY TO EXIT");
+                            Environment.Exit(0);
+                            return false;
+                        default:
+                            return MainMenu();                //END SECONDARY SWITCH STATEMENT
+                        }     
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("VIEW ALL EVENTS");
+                    Console.WriteLine("1) Continue");
+                    Console.WriteLine("2) Return to MAIN MENU");
+                    Console.WriteLine("3) EXIT APPLICATION");
+                    Console.WriteLine("invalid option will revert to MAIN MENU");
+                    Console.Write("\r\nSelect an option: ");
+                        switch (Console.ReadLine())     //SECONDARY SWITCH STATEMENT
+                {
+                        case "1":
+                            Console.WriteLine("CURRENT ENTRIES");
+                        Console.WriteLine(File.ReadAllText(@"example.txt"));
+                            return false;
+                        case "2":
+                            return true;
+                        case "3":
+                            Console.Clear();
+                            Console.WriteLine("PRESS ANY KEY TO EXIT");
+                            Environment.Exit(0);
+                            return false;
+                        default:
+                            return MainMenu();      //END SECONDARY SWITCH STATEMENT
+                }
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("PRESS ANY KEY TO EXIT");
+                    Environment.Exit(0);
+                    return false;
+                default:
+                    return true;        //END FIRST SWITCH STATEMENT
+            }
+        }
+    
+
     }
