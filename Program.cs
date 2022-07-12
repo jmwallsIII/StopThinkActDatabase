2dd8e2a5ed06b13127b94e4a512679eb87124f90
Gina Poynter <johnmurrellwalls@yahoo.com>
2022-07-12 03:32:15 -0400

    modified:   .vs/ProjectEvaluation/stopthinkact.metadata.v2 	modified:   .vs/ProjectEvaluation/stopthinkact.projects.v2 	modified:   .vs/StopThinkAct/DesignTimeBuild/.dtbcache.v2 	deleted:    .vs/StopThinkAct/FileContentIndex/17ac29cc-281d-4fea-99f4-380c71090879.vsidx 	deleted:    .vs/StopThinkAct/FileContentIndex/e18df111-d6b6-4f20-999b-b412d2891aae.vsidx 	deleted:    .vs/StopThinkAct/FileContentIndex/fcdbe9fb-c03b-4460-bc93-1b4492f8272d.vsidx 	modified:   .vs/StopThinkAct/v17/.futdcache.v1 	modified:   .vs/StopThinkAct/v17/.suo 	modified:   Program.cs 	modified:   bin/Debug/net6.0/StopThinkAct.dll 	modified:   bin/Debug/net6.0/StopThinkAct.pdb 	modified:   bin/Debug/net6.0/example.txt 	modified:   obj/Debug/net6.0/StopThinkAct.GeneratedMSBuildEditorConfig.editorconfig 	modified:   obj/Debug/net6.0/StopThinkAct.assets.cache 	modified:   obj/Debug/net6.0/StopThinkAct.csproj.FileListAbsolute.txt 	modified:   obj/Debug/net6.0/StopThinkAct.dll 	modified:   obj/Debug/net6.0/StopThinkAct.genruntimeconfig.cache 	modified:   obj/Debug/net6.0/StopThinkAct.pdb 	modified:   obj/Debug/net6.0/ref/StopThinkAct.dll 	modified:   obj/Debug/net6.0/refint/StopThinkAct.dll 	modified:   obj/StopThinkAct.csproj.nuget.dgspec.json 	modified:   obj/StopThinkAct.csproj.nuget.g.props 	modified:   obj/project.assets.json 	modified:   obj/project.nuget.cache

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
-﻿    internal class Program
-    {
+﻿namespace StopThinkAct
+{
 
+
+    internal class Program
+    {
 
         static void Main(string[] args)
         {
-      
             bool showMenu = true;
-                while (showMenu)
-                {
+            while (showMenu)
+            {
                 showMenu = MainMenu();
-                }
-
-            
+            }
         }
-        
-    
+
+        static string TimeReference()
+        {
+            string now = DateTime.Now.ToString();
+            return now;
+        }
         static bool MainMenu()
         {
             Console.Clear();
@@ -26,22 +30,32 @@
 
             switch (Console.ReadLine())     //FIRST SWITCH STATEMENT
             {
-                case "1":     
-                    Console.Clear ();
+                case "1":
+                    Console.Clear();
                     Console.WriteLine("ADD NEW EVENT:");
                     Console.WriteLine("1) Continue");
                     Console.WriteLine("2) Return to MAIN MENU");
                     Console.WriteLine("3) EXIT APPLICATION");
                     Console.WriteLine("invalid option will revert to MAIN MENU");
                     Console.Write("\r\nSelect an option: ");
-                        switch (Console.ReadLine())     //SECONDARY SWITCH STATEMENT
-                        {
+                    switch (Console.ReadLine())     //SECONDARY SWITCH STATEMENT
+                    {
                         case "1":
                             Console.Clear();
                             Console.WriteLine("PLEASE INCLUDE THE FOLLOWING DETAILS:");
-                        File.AppendAllText(@"example.txt", Console.ReadLine() + Environment.NewLine);
-                        Console.ReadLine();
-                            return false;             
+                            Console.WriteLine("1) A brief description of what was observed" + Environment.NewLine + "2) Causes or contributing factors" + Environment.NewLine + "3) Actions taken or improvement suggestions" + Environment.NewLine);
+                            Console.WriteLine("NOTE: Press enter key after input for each requested detail" + Environment.NewLine);
+                            Console.WriteLine("Proceed:");
+                            Console.Write("1."); File.AppendAllText(@"example.txt", TimeReference() + Environment.NewLine + "1." + Console.ReadLine() + Environment.NewLine);
+                            Console.Write("2."); File.AppendAllText(@"example.txt", "2." + Console.ReadLine() + Environment.NewLine);
+                            Console.Write("3."); File.AppendAllText(@"example.txt", "3." + Console.ReadLine() + Environment.NewLine);
+                            Console.Clear();
+                            Console.WriteLine("Thank you!  Press ENTER key to return to MAIN MENU");
+                            switch (Console.ReadLine())    //tertiary switch 
+                            {
+                                default:
+                                    return MainMenu();   //tertiary switch end
+                            }
                         case "2":
                             return true;
                         case "3":
@@ -51,7 +65,7 @@
                             return false;
                         default:
                             return MainMenu();                //END SECONDARY SWITCH STATEMENT
-                        }     
+                    }
                 case "2":
                     Console.Clear();
                     Console.WriteLine("VIEW ALL EVENTS");
@@ -60,12 +74,18 @@
                     Console.WriteLine("3) EXIT APPLICATION");
                     Console.WriteLine("invalid option will revert to MAIN MENU");
                     Console.Write("\r\nSelect an option: ");
-                        switch (Console.ReadLine())     //SECONDARY SWITCH STATEMENT
-                {
+                    switch (Console.ReadLine())     //SECONDARY SWITCH STATEMENT
+                    {
                         case "1":
-                            Console.WriteLine("CURRENT ENTRIES");
-                        Console.WriteLine(File.ReadAllText(@"example.txt"));
-                            return false;
+                            Console.Clear();
+                            Console.WriteLine("START CURRENT ENTRIES:");
+                            Console.WriteLine(File.ReadAllText(@"example.txt"));
+                            Console.WriteLine("END CURRENT ENTRIES:  Press ENTER key to return to MAIN MENU");
+                            switch (Console.ReadLine())    //tertiary switch 
+                            {
+                                default:
+                                    return MainMenu();   //tertiary switch end
+                            }
                         case "2":
                             return true;
                         case "3":
@@ -75,8 +95,8 @@
                             return false;
                         default:
                             return MainMenu();      //END SECONDARY SWITCH STATEMENT
-                }
-                case "3":
+                    }
+                case "3":           //FIRST SWITCH STATEMENT
                     Console.Clear();
                     Console.WriteLine("PRESS ANY KEY TO EXIT");
                     Environment.Exit(0);
@@ -85,6 +105,6 @@
                     return true;        //END FIRST SWITCH STATEMENT
             }
         }
-    
 
     }
+}
