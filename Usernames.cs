95709bbc8f1dfab59370fbc4808581a3ae823a67
John Walls <johnmurrellwalls@yahoo.com>
2022-07-20 15:12:05 -0400

    new file:   Usernames.cs 	new file:   bin/Debug/net6.0/eventsfile.txt

---
+++ Usernames.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopThinkAct
+{
+    public class Usernames
+    {
+        public string user1 = "john";
+        public string user2 = "stew";
+        public string user3 = "tester";
+
+        public void SelectionPrompt()
+        {
+         
+            Console.WriteLine("Choose a user");
+            Console.WriteLine("1" + user1);
+            Console.WriteLine("2" + user2);
+            Console.WriteLine("3" + user3);
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("");
+                    return;
+                    case "2":
+                    return;
+                    case"3":
+                    return;
+                default:
+                    return SelectionPrompt();
+
+            }
+        }
+    }
+}
